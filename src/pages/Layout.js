1ec4206b7d8695b53c5d5ce20b5aa518567ab022
Johnny Figueroa <johnnyfig00@yahoo.com>
2023-01-03 11:40:01 -0500

    navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,13 +1,13 @@
 import { Outlet, Link } from 'react-router-dom';
+import './styles/layout.css';
 
 const Layout = () => {
     return (
         <>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/TicTacToe">TicTacToe</Link></li>
-                    <li><Link to="/About">About</Link></li>
+            <nav className='navbar'>
+                <ul className='fullNav'>
+                    <li><Link to="/" className='item'>Home</Link></li>
+                    <li><Link to="/About" className='item'>About</Link></li>
                 </ul>
             </nav>
 
